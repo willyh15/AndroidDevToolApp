818ce352d6910117b40ebadd290c64a07937090d
Will Harrison <willharrison2016@gmail.com>
2024-09-05 20:52:29 -0500

    Update androiddevtoolapp

---
+++ app/src/main/java/com/example/androiddevtoolapp/Workflow.java
@@ -0,0 +1,12 @@
+import com.google.gson.annotations.SerializedName;
+
+public class Workflow {
+
+    @SerializedName("id")
+    private long id;
+
+    @SerializedName("name")
+    private String name;
+
+    // Getters and setters
+}
