90f9f3603c4c13290038b1b5622672b44ae7b4e5
Will Harrison <willharrison2016@gmail.com>
2024-09-05 20:50:19 -0500

    Update androiddevtoolapp

---
+++ app/src/main/java/com/example/androiddevtoolapp/GitHubRepo.java
@@ -0,0 +1,21 @@
+import com.google.gson.annotations.SerializedName;
+
+public class GitHubRepo {
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("private")
+    private boolean isPrivate;
+
+    public GitHubRepo(String name, String description, boolean isPrivate) {
+        this.name = name;
+        this.description = description;
+        this.isPrivate = isPrivate;
+    }
+
+    // Getters and setters
+}
