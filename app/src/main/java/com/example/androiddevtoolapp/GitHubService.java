a7894fdae4f5ed9a98b9a488e698ccf6b7e8e54f
Will Harrison <willharrison2016@gmail.com>
2024-09-07 15:53:02 -0500

    Update GitHubService.java

--- app/src/main/java/com/example/androiddevtoolapp/GitHubService.java
+++ app/src/main/java/com/example/androiddevtoolapp/GitHubService.java
@@ -1,9 +1,10 @@
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
@@ -19,6 +20,14 @@ public interface GitHubService {
     Call<WorkflowsResponse> listWorkflows(@Path("owner") String owner, @Path("repo") String repo);
 
     // Trigger workflow
+    @Headers("Accept: application/vnd.github.v3+json")
+    @POST("/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches")
+    Call<Void> triggerWorkflow(@Path("owner") String owner,
+                               @Path("repo") String repo,
+                               @Path("workflow_id") String workflowId,
+                               @Body WorkflowDispatch body);
+
+    // Upload file
     @Headers("Accept: application/vnd.github.v3+json")
     @PUT("/repos/{owner}/{repo}/contents/{path}")
     Call<Void> uploadFile(@Path("owner") String owner,
