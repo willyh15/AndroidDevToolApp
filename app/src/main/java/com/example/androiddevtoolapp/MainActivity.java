50d009c2d50b08d64f5cfa70de28d739c11d0f79
Will Harrison <willharrison2016@gmail.com>
2024-09-07 02:25:35 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/androiddevtoolapp/MainActivity.java
+++ app/src/main/java/com/example/androiddevtoolapp/MainActivity.java
@@ -1,1 +1,30 @@
-creation
+package com.example.androiddevtoolapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Load the default fragment (e.g., Commands Fragment)
+        loadFragment(new CommandsFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    // Call this function when you want to switch to another fragment
+    public void switchFragment(Fragment fragment) {
+        loadFragment(fragment);
+    }
+}
