ecc9351ddb345d07bff7fc6febb7c99b33cce41a
Will Harrison <willharrison2016@gmail.com>
2024-09-07 16:06:33 -0500

    update

--- app/src/main/java/com/example/androiddevtoolapp/Workflow.java
+++ app/src/main/java/com/example/androiddevtoolapp/Workflow.java
@@ -9,4 +9,19 @@ public class Workflow {
     private String name;
 
     // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
